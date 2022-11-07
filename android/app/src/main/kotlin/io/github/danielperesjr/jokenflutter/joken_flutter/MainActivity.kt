ce46706efa57f0a59db4eef5cd086d532f0725b2
Daniel Peres Jr <danielperes.junior@gmail.com>
2022-11-07 10:47:13 -0300

    Commit inicial

---
+++ android/app/src/main/kotlin/io/github/danielperesjr/jokenflutter/joken_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.danielperesjr.jokenflutter.joken_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Jogo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Jogo extends StatefulWidget {
+  const Jogo({Key? key}) : super(key: key);
+
+  @override
+  State<Jogo> createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
