60e852e25bc3d2b37e123fed0cd24dbfd3400b4f
Daniel Peres Jr <danielperes.junior@gmail.com>
2022-11-07 11:18:09 -0300

    Estrutura inicial da interface

--- lib/Jogo.dart
+++ lib/Jogo.dart
@@ -10,6 +10,46 @@ class Jogo extends StatefulWidget {
 class _JogoState extends State<Jogo> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("JokenFlutter"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 50, bottom: 50),
+            child: Text(
+              "Escolha do App:",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Image.asset("images/padrao.png"),
+          Padding(
+            padding: EdgeInsets.only(top: 50, bottom: 50),
+            child: Text(
+              "Escolha uma opção abaixo:",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset("images/pedra.png", height: 100),
+              Image.asset("images/papel.png", height: 100),
+              Image.asset("images/tesoura.png", height: 100)
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
